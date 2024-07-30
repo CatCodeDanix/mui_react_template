48185ca1e1489944ce7247d329b330c2dd67f16b
CatCodeDanix <abdolidanial@gmail.com>
2024-07-30 22:16:37 +0330

    feat: add react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { CacheProvider } from "@emotion/react";
 import { StyledEngineProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -29,6 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <ThemeProvider>
             <QueryClientProvider client={queryClient}>
               <App />
+              <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
           </ThemeProvider>
         </StyledEngineProvider>
